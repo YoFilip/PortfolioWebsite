8e87d140c8dc1c2de287c6827f5d1f50e62e2908
Yo FIlip <102390337+YoFilip@users.noreply.github.com>
2025-03-06 17:11:36 +0100

    Edit config to deplay site to hosting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  //To deplay to hosting, remove the  path to
+  // base: '/ ',
   base: '/PortfolioWebsite/ ',
 })
